84ec817a14a5b79be12ff266d9df6a9a09f1014b
yeswanthmaturi <137012121+yeswanthmaturi@users.noreply.github.com>
2025-06-17 19:11:48 -0700

    Add typewriter effect to 'Succeed or don't pay us' text

--- src/pages/ExternalMetaPage.tsx
+++ src/pages/ExternalMetaPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ArrowLeft, Code, ExternalLink } from 'lucide-react';
+import Typewriter from 'typewriter-effect';
 
 function ExternalMetaPage() {
   const handleBackToHome = () => {
@@ -36,7 +37,25 @@ function ExternalMetaPage() {
             </div>
             <div className="mb-10 max-w-2xl space-y-4 text-2xl font-bold tracking-tight">
               <div className="decisive-edge">
-                We are hands down the best resource available online for the Meta Data Engineer interview.     <span className="text-3xl">  Succeed or don't pay us*</span>
+                We are hands down the best resource available online for the Meta Data Engineer interview.     <span className="text-3xl">
+                  <Typewriter
+                    onInit={(typewriter) => {
+                      typewriter
+                        .typeString('Succeed or don\'t pay us*')
+                        .start();
+                    }}
+                    options={{
+                      cursor: '|',
+                      delay: 75,
+                      loop: false,
+                      autoStart: true,
+                      deleteSpeed: Infinity,
+                      wrapperClassName: 'Typewriter__wrapper',
+                      cursorClassName: 'Typewriter__cursor',
+                      keepCursorOnComplete: true
+                    }}
+                  />
+                </span>
               </div>
             </div>
             
